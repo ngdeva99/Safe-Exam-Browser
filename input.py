4c6fa7c838431c9693d084a0e667a5e5a632eee9
Devanathan N G <31466229+ngdeva99@users.noreply.github.com>
2020-08-15 18:12:12 +0530

    Update input.py

--- input.py
+++ input.py
@@ -13,25 +13,24 @@ from shutil import copyfile
 
 
 def myfun():
-	software = 'SEB_3.0.1.163_SetupBundle.exe'
+	software = ''
 
-	renamed_software = 'SSN_EXAM.exe'
+	renamed_software = ''
 
-	seb_software = 'SebClientSettings.seb'
+	seb_software = ''
 
-	seb_renamed_software = 'SSN_EXAM_SETTINGS.seb'
-
-
+	seb_renamed_software = ''
 
 
 
-	print("WELCOME TO SSN CAMPUS EXAM 2020")
+
+
+	print("WELCOME TO EXAMINATION PORTAL ")
 
 	choices = [False]*2
 
-	#print("The path of Your Download directory is:")
 	downloads_path = str(Path.home())+"\\"+"Downloads\\"
-	#like C:/Users/Messi/Downloads/
+
 	def repl(downloads_path):
 		path = []
 		for i in downloads_path:
@@ -66,19 +65,19 @@ def myfun():
 		#print(path+'SebClientSettings.seb',seb_path1+'SebClientSettings.seb')
 
 
-		copyfile(path+'SSN_EXAM_SETTINGS.seb',seb_path1+'SSN_EXAM_SETTINGS.seb')
+		copyfile(path+'SebClientService.seb',seb_path1+'SebClientService.seb')
 
 	def backend(zoom_path):
 
 
 
 
-		file = md.parse(path+"SSN_EXAM_SETTINGS.seb")
+		file = md.parse(path+"SebClientService.seb")
 		value = file.getElementsByTagName("string")[ 28 ].childNodes[ 0 ].nodeValue
 		#print(value)
 		file.getElementsByTagName( "string" )[ 28 ].childNodes[ 0 ].nodeValue=zoom_path
    
-		with open(path+"SSN_EXAM_SETTINGS.seb", "w" )as fs:
+		with open(path+"SebClientService.seb", "w" )as fs:
 			fs.write(file.toxml())
 			fs.close()
 
@@ -88,21 +87,21 @@ def myfun():
 		#print("Kindly cross-check if "+software+" file is Downloaded in the location "+path)
 
 		choices[1] = True
-		os.startfile(path+renamed_software)
+		os.startfile(path+software)
 		return 
 
 	def install1():
 		#print("Kindly cross-check if "+software+" file is Downloaded in the location "+path)
 
 		choices[1] = True
-		os.startfile(path+renamed_software)
+		os.startfile(path+software)
 
 		print("Was the Installation succesful? Type (y/n) letter y for Yes and n for no and hit Enter")
 		i = input()
 		if i=='y':
 			return
 		else:
-			print('Kindly delete the '+renamed_software+' file available in location '+path+' and restart this application again.')
+			print('Kindly delete the '+software+' file available in location '+path+' and restart this application again.')
 			return 
 
 	####################
@@ -126,25 +125,17 @@ def myfun():
 
 			choices[0] = True
 			#webbrowser.open('https://sourceforge.net/projects/seb/files/latest/download')
-			webbrowser.open('https://drive.google.com/file/d/1uOVFdDlYzn-nwdTj9Kt59pwBjr151VZN/view?usp=sharing')
-			webbrowser.open('https://drive.google.com/file/d/1XVzUmcVThWhi-EIuVfC-gpuIifZ_kZtS/view?usp=sharing')
-		#elif choice ==2:
+			webbrowser.open('https://sourceforge.net/projects/seb/files/latest/download')#FILE LINK TO DOWNLOAD THE SOFTWARE
+			webbrowser.open('')#Specify the location to access the .seb file link
 
 
 			print("\n Please wait for the Download to finish!")
 			while not os.path.isfile(path+software) or not os.path.isfile(path+seb_software):
-				#if not choices[0]:
-				#else:
 				
 				continue
-
-			#if choices[1]:
-			#	print("Kindly Check if the software is already installed")
-			#	continue
-
-
-			os.rename(path+seb_software,path+seb_renamed_software)
-			os.rename(path+software,path+renamed_software)
+				
+			#os.rename(path+seb_software,path+seb_renamed_software)
+			#os.rename(path+software,path+renamed_software)
 
 			install()
 
@@ -163,11 +154,11 @@ def myfun():
 	backend(zoom_path)
 
 
-	print("\n Thank You for choosing SSN.")
+	print("\n Thank You for choosing us.")
 	time.sleep(5)
 
 myfun()
-# In[ ]:
+
 
 
 
